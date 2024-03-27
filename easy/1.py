482a37fa8c19c525c30cacc4b1b31f4eb9599104
Eddie <48805547+Eddie9858@users.noreply.github.com>
2024-03-27 14:19:04 +0900

    Create 1 Solution
    Create 1 Solution With Link

---
+++ easy/1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # create a dict for adding num_to_index
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            # target - num to find the number we are looking for 
+            # unless need to use two for loops 
+            complement = target - num
+            # if there is answer of complement in dict return it
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            # if not add it to the dict
+            num_to_index[num] = i
+          
+# https://leetcode.com/problems/two-sum/
