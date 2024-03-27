80e0497a572d4d1cc4ca9d70be3eb629cb220aa8
Eddie <48805547+Eddie9858@users.noreply.github.com>
2024-03-27 14:47:43 +0900

    Create 1071 Solution
    Create 1071 Solution

---
+++ easy/1071.py
@@ -0,0 +1,15 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # find GCD
+        gcd_length = math.gcd(len(str1), len(str2))
+        
+        potential_gcd_str = str1[:gcd_length]
+      
+        # check potential gcd str is right or not
+        if potential_gcd_str * (len(str1) // gcd_length) == str1 and potential_gcd_str * (len(str2) // gcd_length) == str2:
+            return potential_gcd_str
+        else:
+            return ""
+
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/submissions/1215113772?envType=study-plan-v2&envId=leetcode-75
