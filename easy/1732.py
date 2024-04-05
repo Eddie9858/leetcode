648ff8e5315acf0f51b2bcfa912847f71b699d57
Eddie <48805547+Eddie9858@users.noreply.github.com>
2024-04-05 09:37:27 +0900

    Update 1732.py
    Add solution with list

--- easy/1732.py
+++ easy/1732.py
@@ -2,11 +2,17 @@ class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         alti = 0
         max_al = 0
+        # max_al = []
+    
         for i in gain:
             alti += i
             max_al = max(alti, max_al)
+            # max_al.append(alti)
+        # return max(max_al) if max(max_al) > 0 else 0
 
         return max_al
+
+
 
 # https://leetcode.com/problems/find-the-highest-altitude/?envType=study-plan-v2&envId=leetcode-75
 # https://leetcode.com/problems/find-the-highest-altitude/submissions/1223537021?envType=study-plan-v2&envId=leetcode-75
