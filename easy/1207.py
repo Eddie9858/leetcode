63f99b7f0ba461eb6c93820a026c833f6c4e68ce
Eddie <48805547+Eddie9858@users.noreply.github.com>
2024-04-09 20:12:14 +0900

    Create 1207 solution
    Create 1207 solution

---
+++ easy/1207.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        element_counts = Counter(arr)
+        
+        # counts = list(element_counts.values())
+        # return len(counts) == len(set(counts))
+        
+        return len(element_counts.values()) == len(set(element_counts.values()))
+            
+# https://leetcode.com/problems/unique-number-of-occurrences/description/?envType=study-plan-v2&envId=leetcode-75
+# https://leetcode.com/problems/unique-number-of-occurrences/submissions/1227562028?envType=study-plan-v2&envId=leetcode-75
+
+
